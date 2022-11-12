348f1376405b7873b0f0f7a80c8a82b009aae5dc
Jinseon Choi <oooihmm@naver.com>
2022-11-12 21:39:32 +0900

    Feat: nav바 제작

---
+++ frontend/src/components/Frame.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="body">
+    <header>
+      <div class="nav" id="logo">
+        <router-link to="/">
+          <img src="@/assets/logo.png" id="logoImg">
+        </router-link>
+      </div>
+      <router-link to="/login">
+        <login />
+      </router-link>
+    </header>
+  </div>
+</template>
+
+<script>
+import login from "@/components/loginButton.vue"
+
+export default {
+  components: {
+    login
+  }
+}
+</script>
+
+<style scoped>
+  header {
+    height: 70px;
+    width: 100wh;
+    position:relative;
+    background-color: white;
+  }
+
+  .nav {
+    height: 100%;
+    padding: 0 10px 0 10px;
+
+    float: left;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  #logo {
+    width: 100px;
+    position:absolute;
+    left:50%;
+    margin-left:-50px;
+  }
+
+  #logoImg {
+    height: 50px;
+  }
+</style>
---
+++ frontend/src/components/loginButton.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <button id="login">로그인</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    isNone() {
+      const link = document.location.href
+      const button = document.getElementById('login')
+      if (link === 'http://localhost:8080/') {
+        button.setAttribute('display', 'hidden')
+      }
+    }
+  }
+}
+</script>
+
+<style>
+  button {
+    height: 100%;
+    padding: 0 10px 0 10px;
+
+    float: left;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  #login {
+    display: flex;
+    float: right;
+
+    width: auto;
+    height: 30px;
+    margin: 20px 15px 20px 15px;
+    font-size: 12px;
+    font-weight: 500;
+    color: rgb(255, 142, 162);
+    background-color: white;
+    border: 1px solid rgb(255, 142, 162);
+    border-radius: 15px;
+  }
+
+  #login:hover {
+    color: white;
+    background-color: rgb(255, 142, 162);
+  }
+
+</style>
