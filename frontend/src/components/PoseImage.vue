ae69ef83446f5757b6ca13366d00b491bd59f00a
Jinseon Choi <oooihmm@naver.com>
2022-11-13 04:53:12 +0900

    Feat: main 화면 구현

---
+++ frontend/src/components/PoseImage.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="imageBox">
+    <ul class="wrap">
+      <li class="item" v-for="(pose, idx) in pose" :key="{idx}">
+          <img :src="pose.file">
+          <span class="tit">{{pose.name}}</span>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      pose: [
+        {
+          name: '하트컷',
+          file: require('@/assets/pose/2.jpg'),
+          number: 2
+        },
+        {
+          name: '새싹컷',
+          file: require('@/assets/pose/3.jpg'),
+          number: 2
+        },
+        {
+          name: '연결컷',
+          file: require('@/assets/pose/4.jpg'),
+          number: 2
+        },
+        {
+          name: '끌려가기컷',
+          file: require('@/assets/pose/5.jpg'),
+          number: 2
+        },
+        {
+          name: '산발컷',
+          file: require('@/assets/pose/6.jpg'),
+          number: 2
+        },
+        {
+          name: '친한척컷',
+          file: require('@/assets/pose/7.jpg'),
+          number: 2
+        },
+        {
+          name: '패트와매트컷',
+          file: require('@/assets/pose/8.jpg'),
+          number: 2
+        },
+        {
+          name: '희도유림컷',
+          file: require('@/assets/pose/9.jpg'),
+          number: 2
+        },
+        {
+          name: '패트와매트컷2',
+          file: require('@/assets/pose/10.jpg'),
+          number: 2
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#imageBox {
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+img {
+  height: 150px;
+}
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,17 +1,30 @@
 <template>
   <div id="bigBody">
     <Frame />
-    <div id="body">
+    <div id="main">
+      <span id="ment1">당신께 딱 어울릴</span>
+      <span id="ment2">추천 포즈</span>
+      <img id="randomImg" src="http://13.209.81.198:8083/randomPose">
+      <span id="ment3" @click="click()">다른 포즈가 보고 싶다면?</span>
+      <!-- <PoseImage /> -->
     </div>
   </div>
 </template>
 
 <script>
 import Frame from '@/components/Frame.vue'
+// import PoseImage from '@/components/PoseImage.vue'
+import router from '../router'
 
 export default {
   components: {
     Frame
+    // PoseImage
+  },
+  methods: {
+    click() {
+      router.go()
+    }
   }
 }
 </script>
@@ -25,9 +38,44 @@ export default {
     min-height:670px;
   }
 
-  #body {
-    height: 100%;
+  #main {
+    height: 100vh;
     overflow: scroll;
     background-color: rgb(255, 142, 162);
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  span {
+    color: white;
+  }
+
+  #ment1 {
+    margin-top: 5%;
+  }
+
+  #ment2 {
+    color: black;
+    font-size: 50px;
+    font-weight: 800;
+  }
+
+  #ment3 {
+    background-color: black;
+    /* color: rgb(255, 142, 162); */
+    color: white;
+
+    padding: 10px;
+    margin: 5%;
+    font-weight: 800;
+    border: 1px solid rgb(255, 142, 162);
+    border-radius: 15px;
+  }
+
+  #randomImg {
+    width: 90%;
+    margin: 5% 5% 0px 5%;
   }
 </style>
